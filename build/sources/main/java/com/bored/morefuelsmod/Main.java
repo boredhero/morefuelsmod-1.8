541ca3943f7753fd4df85fdd23873f946dd1722e
boredherobrine13 <treeclan12@gmail.com>
2016-04-18 22:03:15 -0400

    Compiled v1.3.2 - The Sexy Update for Minecraft 1.8.9

--- build/sources/main/java/com/bored/morefuelsmod/Main.java
+++ build/sources/main/java/com/bored/morefuelsmod/Main.java
@@ -17,7 +17,7 @@ import com.bored.morefuelsmod.Fuels;
 public class Main {
 
 	public static final String MODID = "morefuelsmod-1.8.9";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 	public static final String MODNAME = "More Fuels Mod";
 	
 	@Instance(value = Main.MODID)
@@ -31,6 +31,8 @@ public class Main {
 		if(enableRFtLrecipe)
 			GameRegistry.addSmelting(Items.rotten_flesh, new ItemStack(Items.leather), 0.3F);
 		config.save();
+		//Load the items.
+		//MoreFuelsItems.createItems();
 		}
 
 	
